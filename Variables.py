5da3fc938069c078806215c657048c0ba11cd783
Dreams-github <124748152+Dreams-github@users.noreply.github.com>
2023-02-07 22:14:25 -0600

    Creating variables

---
+++ Variables.py
@@ -0,0 +1,14 @@
+a=7667 #int
+b=3424.2342 #float
+
+add=a+b
+print("addition is",add)
+
+sub=a-b
+print("substraction is",sub)
+
+div=a/b
+print("division is",div)
+
+mult=a*b
+print("multiplication is",mult)
