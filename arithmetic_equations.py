b4d885e22a00b91024a601a86ebce79424ec6876
Dreams-github <124748152+Dreams-github@users.noreply.github.com>
2023-02-07 22:16:59 -0600

    Create arithmetic_equations.py

---
+++ arithmetic_equations.py
@@ -0,0 +1,20 @@
+a1= 15
+b1= 20
+c1= 10
+d1= 30
+
+add= a1+d1
+print("addition of ",a1,"+",d1,"=",add)
+
+sub= b1-c1
+print("subraction of ",b1,"-",c1,"=", sub)
+
+div= c1/a1
+print("division of", c1,"/",a1,"=",div)
+
+mul= c1*d1
+print("multiplication of", c1,"*",d1,"=",mul)
+
+e=((d1+a1)-c1*a1)/b1
+
+print("((",d1,"+",a1,")","-",c1,"*",a1,")","/",b,"=",e) 
